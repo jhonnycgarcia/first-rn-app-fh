e295ffdc79462081712b3c651c9fa9bae1acf3e7
Jhonny <jhonnycgarcia@gmail.com>
2025-04-11 00:19:29 -0500

    iconos en react native - android ios

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
+import Icon from '@react-native-vector-icons/fontawesome6';
 
 export function App() {
   return (
--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,21 +2,25 @@ import { View, Text } from 'react-native';
 import { useState } from 'react';
 import { globalStyles } from '../theme/global.styles';
 import { FAB } from 'react-native-paper';
+import Icon from '@react-native-vector-icons/fontawesome6';
 
 export const CounterM3Screen = () => {
 
-    const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(10);
+
+  const buttonIcon = () => {
+    return <Icon name="plus" size={24} iconStyle="solid" />;
+  };
 
   return (
     <View style={globalStyles.centerContainer}>
-        <Text style={globalStyles.title}>{counter}</Text>
-        <FAB
-          label="+1"
-          style={globalStyles.fab}
-          onPress={() => setCounter(counter + 1)}
-          onLongPress={() => setCounter(0)}
-        />
+      <Text style={globalStyles.title}>{counter}</Text>
+      <FAB
+        icon={buttonIcon}
+        style={globalStyles.fab}
+        onPress={() => setCounter(counter + 1)}
+        onLongPress={() => setCounter(0)}
+      />
     </View>
   );
 };
-
