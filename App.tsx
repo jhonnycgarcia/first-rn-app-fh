f0752183804f93851ff62168b0c75b5887c500d0
Jhonny <jhonnycgarcia@gmail.com>
2025-04-10 22:55:44 -0500

    floating action button - FAB

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { SafeAreaView } from 'react-native';
-import { CounterScreen } from './src/presentation/screens/CounterScreen';
 import { PaperProvider } from 'react-native-paper';
+import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
 
 export function App() {
   return (
     <PaperProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <CounterScreen />
+        <CounterM3Screen />
       </SafeAreaView>
     </PaperProvider>
   );
---
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { globalStyles } from '../theme/global.styles';
+import { FAB } from 'react-native-paper';
+
+export const CounterM3Screen = () => {
+
+    const [counter, setCounter] = useState(10);
+
+  return (
+    <View style={globalStyles.centerContainer}>
+        <Text style={globalStyles.title}>{counter}</Text>
+        <FAB
+          label="+1"
+          style={globalStyles.fab}
+          onPress={() => setCounter(counter + 1)}
+          onLongPress={() => setCounter(0)}
+        />
+    </View>
+  );
+};
+
