fe8b459f82857427389c74599f6359e3aee92594
Jhonny <jhonnycgarcia@gmail.com>
2025-04-11 20:29:20 -0500

    align items

--- src/presentation/screens/FlexDirectionScreen.tsx
+++ src/presentation/screens/FlexDirectionScreen.tsx
@@ -15,22 +15,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#d1d1d1',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'stretch',
     flexDirection: 'row',
   },
   box: {
-    width: 100,
-    height: 100,
+    flex: 1,
+    // width: 100,
+    // height: 100,
   },
   box1: {
     backgroundColor: '#5856d6',
-    top: -100,
   },
   box2: {
     backgroundColor: '#4240a2',
   },
   box3: {
     backgroundColor: '#2e2d71',
-    bottom: -100,
   },
 });
