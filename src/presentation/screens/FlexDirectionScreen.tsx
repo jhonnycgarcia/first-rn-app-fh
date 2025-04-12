7216e63ff316285d05f254053e57cc039dbcc02f
Jhonny <jhonnycgarcia@gmail.com>
2025-04-11 20:33:35 -0500

    align self

--- src/presentation/screens/FlexDirectionScreen.tsx
+++ src/presentation/screens/FlexDirectionScreen.tsx
@@ -6,6 +6,7 @@ export const FlexDirectionScreen = () => {
         <View style={[ styles.box, styles.box1]} />
         <View style={[ styles.box, styles.box2]} />
         <View style={[ styles.box, styles.box3]} />
+        <View style={[ styles.box, styles.box4]} />
     </View>
   );
 };
@@ -19,17 +20,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   box: {
-    flex: 1,
-    // width: 100,
-    // height: 100,
+    width: 100,
+    height: 100,
   },
   box1: {
     backgroundColor: '#5856d6',
   },
   box2: {
     backgroundColor: '#4240a2',
+    alignSelf: 'flex-end',
   },
   box3: {
     backgroundColor: '#2e2d71',
+  },
+  box4: {
+    backgroundColor: '#222155',
   },
 });
