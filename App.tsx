ff2a8914b571f5859e4ee852ba72cbd7ecdf1138
Jhonny <jhonnycgarcia@gmail.com>
2025-04-11 20:19:48 -0500

    flex

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { SafeAreaView } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { PositionScreen } from './src/presentation/screens';
+import { FlexScreen } from './src/presentation/screens';
 
 export function App() {
   return (
     <PaperProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <PositionScreen />
+      <SafeAreaView style={ {flex: 1} }>
+        <FlexScreen />
       </SafeAreaView>
     </PaperProvider>
   );
--- src/presentation/screens/index.ts
+++ src/presentation/screens/index.ts
@@ -2,5 +2,6 @@ export * from './BoxObjectModelScreen';
 export * from './CounterM3Screen';
 export * from './CounterScreen';
 export * from './DimensionScreen';
+export * from './FlexScreen';
 export * from './HelloWorldScreen';
 export * from './PositionScreen';
